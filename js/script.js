f6d06cc72850ec72e58dde00831bd026c111eda3
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-11 15:54:44 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -1,11 +1,4 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// il prezzo del biglietto è definito in base ai km (0.21 € al km);
-// va applicato uno sconto del 20% per i minorenni;
-// va applicato uno sconto del 40% per gli over 65.
-
 var chilometri = prompt('Quanti chilometri desideri fare?');
-console.log(chilometri);
 var anni = prompt('Quanti anni hai?');
 var prezzoAlChilometro = 0.21;
 var minorenni = 18;
